5d5fcfc370b89dd51838e386ee97b0ca08212ec3
Amirul Fikri <amirul.rostam93@gmail.com>
2019-01-26 11:19:08 +0800

    split js files into its own component files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Movie from './Movie';
+
+const App = function () {
+    return React.createElement("div", {}, [
+        React.createElement(
+            "div", {
+                className: "title-bar"
+            },
+            React.createElement("h1", {}, "React Movie App")
+        ),
+        React.createElement(Movie, {
+            name: "Aquaman",
+            releaseDate: "2018-12-07",
+            genre: "Action"
+        }),
+        React.createElement(Movie, {
+            name: "Bumblebee",
+            releaseDate: "2018-12-15",
+            genre: "Action"
+        }),
+        React.createElement(Movie, {
+            name: "Fantastic Beasts: The Crimes of Grindelwald",
+            releaseDate: "2018-11-14",
+            genre: "Action"
+        })
+    ]);
+};
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+const Movie = props =>
+    React.createElement(
+        "div", {
+            className: "movie-container"
+        },
+        [
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.releaseDate),
+            React.createElement("span", {}, props.genre)
+        ]
+    );
+
+
+export default Movie;
